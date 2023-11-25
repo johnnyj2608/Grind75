7a6d286e078606c66c2a8f50333df0632a6b46aa
Johnny Jiang <johnnyj2608@gmail.com>
2023-11-25 13:21:27 -0500

    Added alternative solutions

--- Week3/counting-bits.py
+++ Week3/counting-bits.py
@@ -8,3 +8,18 @@ class Solution(object):
         for i in range(1, n+1):
             res.append(res[i >> 1] + i % 2)
         return res
+
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = [0] * (n+1)
+        offset = 1
+
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset *= 2
+            res[i] = 1+res[i-offset]
+        return res
--- Week3/missing-number.py
+++ Week3/missing-number.py
@@ -11,10 +11,22 @@ class Solution(object):
             res = res ^ nums[i]
         return res
 
-        # return sum(range(len(nums)+1)) - sum(nums)
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(range(len(nums)+1)) - sum(nums)
 
-        # nums = set(nums)
-        # for i in range(len(nums)):
-        #     if i not in nums:
-        #         return i
-        # return len(nums)
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+        for i in range(len(nums)):
+            if i not in nums:
+                return i
+        return len(nums)
