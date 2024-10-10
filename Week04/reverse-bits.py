fb1eb88c042771ba97ea1bb7bc76adafaaaac395
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2024-10-10 10:43:38 -0400

    2nd Solution

--- Week04/reverse-bits.py
+++ Week04/reverse-bits.py
@@ -7,3 +7,14 @@ class Solution:
             res = (res << 1) + (n & 1)
             n = n >> 1
         return res
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        
+        for i in range(32):
+            res = res * 2 + n % 2
+            n = n >> 1
+        return res
