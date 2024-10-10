c1e1cff459c5182a8784fa52dce1036d04713700
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2024-10-10 11:09:30 -0400

    2nd Solution

--- Week04/insert-interval.py
+++ Week04/insert-interval.py
@@ -16,3 +16,24 @@ class Solution(object):
                 intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1])
                 intervals.pop(i)
         return intervals
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        intervals.append(newInterval)
+        intervals.sort()
+        
+        res = [intervals[0]]
+        
+        for i in range(1, len(intervals)):
+            if res[-1][1] < intervals[i][0]:
+                res.append(intervals[i])
+            else:
+                res[-1][0] = min(res[-1][0], intervals[i][0])
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+
+        return res
