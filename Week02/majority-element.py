668ae1818084676a11cafec70685bc118c90db24
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2025-04-25 11:07:27 -0400

    Replaced with optimized solution

--- Week02/majority-element.py
+++ Week02/majority-element.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return sorted(nums)[len(nums)//2]
+        res = 0
+        majority = 0
+
+        for i in nums:
+            if majority == 0:
+                res = i
+
+            if i == res:
+                majority += 1
+            else:
+                majority -= 1
+        return res
