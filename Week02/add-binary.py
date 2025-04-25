5e9ffb387793eb48f52cd307b014253f2dcb703f
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2025-04-25 13:10:23 -0400

    Second Solution

--- Week02/add-binary.py
+++ Week02/add-binary.py
@@ -21,3 +21,28 @@ class Solution(object):
         if c > 0:
             res += str(c)
         return res[::-1]
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = ''
+        c = 0
+
+        indexA, indexB = len(a)-1, len(b)-1
+
+        while indexA >= 0 or indexB >= 0 or c == 1:
+            if indexA >= 0:
+                c += int(a[indexA])
+                indexA -= 1
+            if indexB >= 0:
+                c += int(b[indexB])
+                indexB -= 1
+            
+            res += str(c % 2)
+            c = c // 2
+
+        return res[::-1]
