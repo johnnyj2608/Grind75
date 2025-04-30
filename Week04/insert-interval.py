826ffe8e52f772cfab167bfd9c1012ba726be57c
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2025-04-29 22:02:26 -0400

    Update insert-interval.py

--- Week04/insert-interval.py
+++ Week04/insert-interval.py
@@ -24,16 +24,16 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        intervals.append(newInterval)
-        intervals.sort()
+        res = []
         
-        res = [intervals[0]]
-        
-        for i in range(1, len(intervals)):
-            if res[-1][1] < intervals[i][0]:
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
             else:
-                res[-1][0] = min(res[-1][0], intervals[i][0])
-                res[-1][1] = max(res[-1][1], intervals[i][1])
-
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
+        
         return res
