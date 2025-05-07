39cbb67b80b9e5c1ecc011668aecaa8ff59904ad
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2025-05-06 23:15:40 -0400

    Update partition-equal-subset-sum.py

--- Week07/partition-equal-subset-sum.py
+++ Week07/partition-equal-subset-sum.py
@@ -19,3 +19,27 @@ class Solution(object):
                 return True
             dp = nextdp
         return False
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+
+        dp = [False] * (target+1)
+        dp[0] = True
+
+        for n in nums:
+            for i in range(target, n-1, -1):
+                if dp[i]:
+                    continue
+                if dp[i-n]:
+                    dp[i] = True
+                if dp[-1]:
+                    return True
+        return False
