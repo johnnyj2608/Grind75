ba9f44093895f4fa28241f3e65830930659f3941
Johnny Jiang <johnnyj2608@gmail.com>
2025-11-13 10:12:11 -0500

    new solutions

--- Week05/number-of-islands.py
+++ Week05/number-of-islands.py
@@ -20,3 +20,25 @@ class Solution(object):
         self.dfs(grid, i, j - 1)
         self.dfs(grid, i, j + 1)
         return 1
+    
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        res = 0
+        
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'
+            for x, y in (1, 0), (-1, 0), (0, 1), (0, -1):
+                dfs(i+x, j+y)
+            return 1
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    res += dfs(i, j)
+        return res
--- Week10/maximum-product-subarray.py
+++ Week10/maximum-product-subarray.py
@@ -11,3 +11,20 @@ class Solution(object):
             mins, maxs = min(i * mins, i * maxs, i), max(i * mins, i * maxs, i)
             res = max(res, mins, maxs)
         return res
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mins, maxs = nums[0], nums[0]
+        res = nums[0]
+
+        for i in nums[1:]:
+            if i < 0:
+                mins, maxs = maxs, mins
+            mins = min(mins*i, i)
+            maxs = max(maxs*i, i)
+            res = max(res, maxs)
+        return res
