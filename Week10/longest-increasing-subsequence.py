57bd42e3047ec24a1b3909b7b67210b950e402a1
johnnyj2608 <54607786+johnnyj2608@users.noreply.github.com>
2025-11-24 11:27:21 -0500

    Add binary search to longest increasing subsequence
    Implement binary search for finding the position in the longest increasing subsequence.

--- Week10/longest-increasing-subsequence.py
+++ Week10/longest-increasing-subsequence.py
@@ -4,6 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        res = []
+
+        def binarySearch(l, r, tar):
+            while l < r:
+
+                m = (r+l)//2
+                if res[m] == tar:
+                    return m
+                if tar < res[m]:
+                    r = m
+                else:
+                    l = m + 1
+            return l
+
+        for i in nums:
+            if len(res) == 0 or res[-1] < i:
+                res.append(i)
+            else:
+                index = binarySearch(0, len(res)-1, i)
+                res[index] = i
+        return len(res)
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         dp = [1] * len(nums)
 
         for i in range(len(nums) - 1, -1, -1):
