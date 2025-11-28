3447315efd7e296879847773b0ec211ae2a25162
Johnny Jiang <johnnyj2608@gmail.com>
2025-11-28 15:10:14 -0500

    Updated solution:

--- Week05/course-schedule.py
+++ Week05/course-schedule.py
@@ -6,6 +6,37 @@ class Solution(object):
         :rtype: bool
         """
         adj = defaultdict(list)
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+
+        cycle = set()
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return True
+            if crs in cycle:
+                return False
+            cycle.add(crs)
+            for i in adj[crs]:
+                if dfs(i) == False:
+                    return False
+            visited.add(crs)
+            return True
+
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        adj = defaultdict(list)
         for pre, crs in prerequisites:
             adj[pre].append(crs)
         visited = set()
